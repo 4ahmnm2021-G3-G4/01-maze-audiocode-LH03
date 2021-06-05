dfd0a6e7218afb06d4536879300c2167e1c443ea
LH03 <lea.helbok03@htl-salzburg.ac.at>
2021-06-05 17:05:58 +0200

    add new scripts for door movement and add interactable scripts to door, frog and basket

---
+++ Assets/MyGame/Scripts/DoorMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+
+
+    [SerializeField] private Vector3 target = new Vector3(1, 1, 1);
+    [SerializeField] private float speed;
+    private bool isPlayerThroughDoor = false;
+    public GameObject door;
+    public AudioSource doorCloseSound;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            isPlayerThroughDoor = true;
+        }
+        // Moves the object to target position
+
+    }
+
+    void Update()
+    {
+        if (isPlayerThroughDoor)
+        {
+            //Debug.Log(target.ToString());
+            door.transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+            doorCloseSound.Play();
+        }
+
+    }
+
+
+}
---
+++ Assets/MyGame/Scripts/FrogCroak.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogCroak : MonoBehaviour
+{
+    public AudioSource frogCroak;
+    public GameObject basket;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Sound off");
+            frogCroak.Stop();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

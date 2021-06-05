51323ec969068b7dfd3b92bad67496fae560194c
LH03 <lea.helbok03@htl-salzburg.ac.at>
2021-06-05 18:10:06 +0200

    show cauldron when three frogs are in basket and stop quacking

---
+++ Assets/MyGame/Scripts/CollectFrogs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectFrogs : MonoBehaviour
+{
+    bool[] frog = new bool[3];
+    public AudioSource frog1;
+    public AudioSource frog2;
+    public AudioSource frog3;
+    public GameObject cauldron;
+        
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Frog 1")
+        {
+            frog[0] = true;
+            frog1.Stop();
+        }
+
+        if (collision.gameObject.tag == "Frog 2")
+        {
+            frog[1] = true;
+            frog2.Stop();
+        }
+
+        if (collision.gameObject.tag == "Frog 3")
+        {
+            frog[2] = true;
+            frog3.Stop();
+        }
+
+        if (frog[0] & frog[1] & frog[2])
+        {
+            cauldron.SetActive(true);
+        }
+    }
+}

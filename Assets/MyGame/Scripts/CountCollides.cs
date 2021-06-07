c4883702d77c324ba85c6c3b5bb7840a0ad20e41
LH03 <lea.helbok03@htl-salzburg.ac.at>
2021-06-07 09:58:09 +0200

    add three times ladle stirring and then the key appears

---
+++ Assets/MyGame/Scripts/CountCollides.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountCollides : MonoBehaviour
+{
+
+    public int counter;
+    public GameObject key;
+    public GameObject invisibleKeyStand;
+    public GameObject rotationSign;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Ladle")
+        {
+            counter++;
+
+            if (counter == 2)
+            {
+                rotationSign.SetActive(false);
+            }
+
+            if (counter == 4)
+            {
+                //Debug.Log("It works");
+                key.SetActive(true);
+                invisibleKeyStand.SetActive(true);
+            }
+
+        }
+    }
+}
+
---
+++ Assets/MyGame/Scripts/StirringCauldron.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StirringCauldron : MonoBehaviour
+{
+    public GameObject TriggerLeft;
+    public GameObject TriggerRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}

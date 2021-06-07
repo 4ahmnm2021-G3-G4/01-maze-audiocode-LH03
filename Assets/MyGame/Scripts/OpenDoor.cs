d40734916fad73a2077c1679cdea99ad2f646cde
LH03 <lea.helbok03@htl-salzburg.ac.at>
2021-06-07 10:36:37 +0200

    add colliders to door and fix minor code mistakes

---
+++ Assets/MyGame/Scripts/OpenDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public int counter;
+    public GameObject key;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == Key)
+        {
+            counter++;
+        }
+
+        if (counter == 1)
+        {
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
